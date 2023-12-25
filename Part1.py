c46ea425aec0107342702e9034e8f4603d721338
maaike <telephonedochter@gmail.com>
2023-12-25 22:57:32 +0100

    comments, docstrings, and refactoring

--- Part1.py
+++ Part1.py
@@ -5,34 +5,53 @@ Students: <person 1>, <person 2>
 """
 # --- Imports ---
 import os
-import re
 import json
-import spacy
-
-
-# import nltk
-# --- You may add other imports here ---
-
-
-# TODO: Load the spaCy model
-# TODO: Load the book text
 
 
 # Function to process the text and perform NER
-def perform_ner(text, nlp):
-    doc=  nlp(text)
+def perform_ner(text: str, nlp) -> list[any]:
+    """
+    Perform Named Entity Recognition (NER) on the input text using the provided spaCy NLP model.
+
+    Args:
+        text (str): The input text to analyze.
+        nlp: A spaCy NLP model with NER capabilities.
+
+    Returns:
+        list: A list of named entities detected in the input text.
+    """
+    doc = nlp(text)
     return list(doc.ents)
 
 
 # Function to extract and structure entity information
-def extract_entity_info(tokens, filename, character_dicts):
+def extract_entity_info(tokens, filename: str, character_dict_list: list):
+    """
+    Extract information about PERSON entities from the given tokens.
+
+    Args:
+        tokens (iterable): A list of spaCy tokens.
+        filename (str): The name of the source file (without the ".txt" extension).
+        character_dict_list (list): A list of dictionaries containing character information.
+
+    Modifies:
+        Updates character_dicts with occurrences of the detected PERSON entities in place.
+    """
+    # iterate through all the tokens
     for token in tokens:
+        # we are only interested in those that have the label 'PERSON'
         if token.label_ == "PERSON":
-            for name in character_dicts:
+            # check if the token that was found was one of the main characters by comparing against the main
+            # character list
+            for name in character_dict_list:
+                # the value for the key 'superstring' is part of the clustering. Whenever a token is a substring of
+                # the identifying superstring, it is handled as a mention of that character
                 if token.text.lower() in name["superstring"]:
-                    if name["superstring"] == "jane_bennet" and token.text == "Bennet":  # to catch when 'Bennet' does not refer to Jane
+                    if name["superstring"] == "jane_bennet" and token.text == "Bennet":  # to catch when 'Bennet'
+                        # does not refer to Jane ('Bennet' can refer to 3 different people, on one will be considered here
                         pass
                     else:
+                        # add all the context information for the identified token to the character's dictionary
                         context_dict = {
                             "sentence": token.sent.text,
                             "chapter": filename.strip(".txt"),
@@ -44,6 +63,17 @@ def extract_entity_info(tokens, filename, character_dicts):
 
 # Function to save data to JSON file
 def save_to_json(character_dicts):
+    """
+    Save the provided character information dictionaries to a JSON file.
+
+    Args:
+        character_dicts (list): A list of dictionaries containing character information.
+
+    Writes:
+        Saves the character information to a JSON file named "pride_and_prejudice_MainCharacters_NER.json"
+        with proper formatting and encoding.
+    """
+    # create the file and store the dictionaries as a json
     with open(".\\pride_and_prejudice\\pride_and_prejudice_MainCharacters_NER.json", "w", encoding="UTF8") as jsonfile:
         json.dump(character_dicts, jsonfile, indent=3)
 
@@ -53,6 +83,7 @@ def main():
     import spacy
     nlp = spacy.load("en_core_web_sm")
 
+# define the basic structure of the dictionaries used to gather info about the main characters
     elizabeth_lizzy = {
         "superstring": "elizabeth_lizzy",
         "name": "Elizabeth Bennet",
@@ -103,20 +134,26 @@ def main():
 
     path = ".\\pride_and_prejudice\\chapters\\"
 
-    character_dicts = [elizabeth_lizzy, jane_bennet, mr_collins, mr_darcy, mr_bingley, lydia,
+    # put the boiler plate dictionaries in a list, to have them handy and be able to access them
+    character_dicts_list = [elizabeth_lizzy, jane_bennet, mr_collins, mr_darcy, mr_bingley, lydia,
                        lady_catherine_de_bourgh]
 
+    # iterate through every chapter of the book to make the analysis
     for filename in os.listdir(path):
+        # 'chapter 0' is not part of the book, but the preface. We are not interested in the sentiments there
         if str(filename) == "0.txt": continue
-        print(filename)
 
+        #open the file and perform the sentiment analysis on that chapter
         with open(f".\\pride_and_prejudice\\chapters\\{filename}", "r", encoding="UTF8") as file:
+
+            # do the analysis with spacy
             tokens = perform_ner(file.read(), nlp)
 
-            extract_entity_info(tokens, filename, character_dicts)
+            # extract the information from the analysed token
+            extract_entity_info(tokens, filename, character_dicts_list)
 
-    save_to_json(character_dicts)
-
+    # save the result as a json
+    save_to_json(character_dicts_list)
 
 
 # Run the main function
--- Part2.py
+++ Part2.py
@@ -4,55 +4,44 @@ Part 0: Book Selection
 Students: <person 1>, <person 2>
 """
 # --- Imports ---
-import os
-import re
 from statistics import mean
 import json
-
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
-# --- You may add other imports here ---
-
-
-# Feel free to add more functions as needed!
-
-
 # Function to perform sentiment analysis
-def analyze_sentiment(text, analyzer):
-    # TODO: Apply the sentiment analysis tool to the text and return the results
-    pass
+def analyze_sentiment(character_dicts, analyzer):
+    """
+    Analyzes sentiment for a list of character occurrences in a book.
 
+    Args:
+    - character_dicts (list of dict): A list of dictionaries, each representing a character's occurrences
+        in the book. Each dictionary should contain an 'occurrences' key with a list of sentences where
+        the character appears.
+    - analyzer: An object with a 'polarity_scores' method that computes sentiment scores for sentences.
 
-# Function to save sentiment analysis results to a JSON file
-def save_sentiment_results(results, filename):
-    # TODO: Save the sentiment analysis results in a structured JSON format
-    pass
+    Returns:
+    None
 
+    This function analyzes the sentiment of sentences associated with each character's occurrences in the book.
+    It calculates the sentiment scores for each sentence and stores them in the 'sentiment' key of each occurrence.
+    It also computes and stores the average sentiment scores per chapter and for the entire book for each character.
 
-def save_to_json(character_dicts):
-    with open(".\\pride_and_prejudice\\pride_and_prejudice_sentiment.json", "w", encoding="UTF8") as jsonfile:
-        json.dump(character_dicts, jsonfile, indent=3)
+    After calling the function, each character dictionary will have sentiment scores and averages.
+    """
 
-
-# Main function
-def main():
-    analyzer = SentimentIntensityAnalyzer()
-
-    # Load the JSON file
-    file_path = '.\\pride_and_prejudice\\pride_and_prejudice_MainCharacters_NER.json'
-
-    with open(file_path, 'r') as file:
-        character_dicts = json.load(file)
-
-    # Iterate over each character and access the 'sentence' property
+    # Iterate over each character and access the 'sentence' property to analyze it
     for character in character_dicts:
+
+        # define data structures to be able to calculate averages over the chapters and the entire book
         compound_entire_book = []
         compound_per_chapter = {}
+
+        # access every occurrence of a character in the book for the sentiment analysis
         for occurrence in character['occurrences']:
             sentence = occurrence["sentence"]
 
-            # add the sentiment score to the dictionary
+            # calculate the sentiment score and add it to the dictionary with a new key
             sentiments = analyzer.polarity_scores(sentence)
             occurrence["sentiment"] = sentiments
 
@@ -60,32 +49,60 @@ def main():
             compound_entire_book.append(compound)
 
             # add the value to the compound per chapter dict
+            # there is a key for every chapter, add to that if there already exists a value for the chapter
+            # if not, add a new key
             try:
                 compound_per_chapter[occurrence["chapter"]].append(compound)
             except:
                 compound_per_chapter[occurrence["chapter"]] = [compound]
 
-        #average per chapter
+        # calculate and store the average per chapter
         for chapter in compound_per_chapter:
             compound_per_chapter[chapter] = mean(compound_per_chapter[chapter])
-        character["compound_average_per_chapter"] = dict(sorted(compound_per_chapter.items(), key=lambda item: int(item[0])))
+        character["compound_average_per_chapter"] = dict(
+            sorted(compound_per_chapter.items(), key=lambda item: int(item[0])))
 
-        #average entire book
+        # calculate and store the average entire book
         compound_average_entire_book = mean(compound_entire_book)
         character["compound_average_entire_book"] = compound_average_entire_book
 
 
+# Function to save sentiment analysis results to a JSON file
+def save_sentiment_results(character_dicts):
+    """
+    Save the provided character information dictionaries to a JSON file.
 
-    save_to_json(character_dicts)
+    Args:
+        character_dicts (list): A list of dictionaries containing character information.
 
-    # TODO: Load or define the text for analysis
-    text = "Your text for sentiment analysis."
+    Writes:
+        Saves the character information to a JSON file named "pride_and_prejudice_MainCharacters_NER.json"
+        with proper formatting and encoding.
+    """
+    # create the file and store the dictionaries as a json
+    with open(".\\pride_and_prejudice\\pride_and_prejudice_sentiment.json", "w", encoding="UTF8") as jsonfile:
+        json.dump(character_dicts, jsonfile, indent=3)
 
-    # TODO: Perform sentiment analysis on the text using your chosen tool
-    # For example, analyze each sentence or paragraph where entities are identified
 
-    # Save the results to a JSON file
-    # Example filename: 'BookTitle_Sentiment.json'
+# Main function
+def main():
+
+    #initialize the vader sentiment analyzer
+    analyzer = SentimentIntensityAnalyzer()
+
+    # Load the JSON file
+    file_path = '.\\pride_and_prejudice\\pride_and_prejudice_MainCharacters_NER.json'
+
+    # open and load the file that came out of part1.py to append it with the sentiment
+    with open(file_path, 'r') as file:
+        character_dicts = json.load(file)
+
+    # execute the anaylsis
+    analyze_sentiment(character_dicts, analyzer)
+
+    # save the results to a new json
+    save_sentiment_results(character_dicts)
+
 
 
 # Run the main function
